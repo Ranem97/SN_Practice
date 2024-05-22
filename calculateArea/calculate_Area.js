082c859ccc5ff346c126bdb01918b229d723825b
Ranem97 <haffarranem@gmail.com>
2024-05-22 16:24:58 +0400

    message

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -10,3 +10,19 @@ function calculateArea() {
     "result"
   ).innerText = `The area of the rectangle is: ${area}`;
 }
+
+let grocery1;
+let grocery2;
+let grocery3;
+
+function calculateAmount() {
+  grocery1 = parseFloat(document.getElementById("grocery1").value);
+  grocery2 = parseFloat(document.getElementById("grocery2").value);
+  grocery3 = parseFloat(document.getElementById("grocery3").value);
+
+  let amountSum = grocery1 + grocery2 + grocery3;
+
+  document.getElementById(
+    "amount"
+  ).innerText = `The total amount is :$ ${amountSum}`;
+}

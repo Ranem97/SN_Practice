d6af6b045a3ca10536ea5b838c89adfd2e326611
Ranem97 <haffarranem@gmail.com>
2024-06-12 15:31:35 +0400

    message

--- speedAnalysis/speed_analysis.js
+++ speedAnalysis/speed_analysis.js
@@ -40,6 +40,8 @@ function endTest() {
   var outputDiv = document.getElementById("output");
   outputDiv.innerHTML =
     "<h2>Typing Test Results:</h2>" +
+    "Total Length:" +
+    userTypedText.length +
     "<p>Words Typed: " +
     typedWords +
     "</p>" +

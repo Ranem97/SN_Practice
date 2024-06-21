3c3d24a4d779cff5f5f5b129b1f96e49795d27a2
Ranem97 <haffarranem@gmail.com>
2024-06-21 19:10:06 +0400

    message

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -2,6 +2,8 @@ const taskInput = document.getElementById("taskInput");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+const clearAllBtn = document.getElementById("clearAllBtn");
+
 let tasks = [];
 
 function addTask() {
@@ -38,6 +40,12 @@ function clearCompletedTasks() {
   displayTasks();
 }
 
+function clearAllTasks() {
+  tasks = [];
+  displayTasks();
+}
+
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+clearAllBtn.addEventListener("click", clearAllTasks);
 displayTasks();
